581f63ee4abe82d339cadd3e1d83957ed940b741
Ashrith Chandramouli <ashrith1516@gmail.com>
2020-02-26 00:42:01 -0500

    Added faster grid filtering to corner detection and removed reports

--- Corner_Detection/corner_detection.py
+++ Corner_Detection/corner_detection.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import ndimage,signal
 from math import sqrt,pi,degrees,atan
+from operator import itemgetter
 import cv2
+import time
 
 #Computes the C matrix - [[Eex,Eexy],[Eexy],[Eey]]
 #Takes in the gradient matrices ex (x gradient squared), ey (y gradient squared), exey (product of x and y gradients),
@@ -18,24 +20,36 @@ def compute_c(ex,ey,exey,i,j,offset):
 	c_matrix = [[sum_ex,sum_exey],[sum_exey,sum_ey]]
 	return c_matrix
 
-#Filters corners based on threshold from possible corner points
+#Filters corners based on threshold from possible corner points.
+#Type linear gives accurate representation of corners. Type grid is 30% faster than linear but has overlapping grids
 #Threshold is calculated based on a percentage of the maximum delta value in the list
 #Takes in the possible corner points, threshold ratio and offset of neighborhood
-def filter_points(points,threshold,offset):
+def filter_points(points,threshold,offset,filter_type):
 	new_points = []
-	#Sort in decreasing order of eigenvalue delta
-	points = sorted(points,key = lambda x : x[2],reverse=True)
-	maxVal = points[0][2]
 	#Compute threshold eigenvalue
+	maxVal = max(points,key=itemgetter(2))[2]
 	threshold = maxVal * threshold
 	points = [x for x in points if x[2] > threshold]
-	
+
+	#Sort in decreasing order of eigenvalue delta
+	points = sorted(points,key = lambda x : x[2],reverse=True)
+
 	#Remove all lower eigenvalue points in the neighbourhood
-	while points:
-		new_points.append(points[0])
-		points = [point for point in points[1:] if point[0] > new_points[-1][0] + offset or point[1] > new_points[-1][1] + offset or point[0] < new_points[-1][0] - offset or point[1] < new_points[-1][1] - offset]
+	if filter_type == "linear":
+		while points:
+			new_points.append(points[0])
+			points = [point for point in points[1:] if point[0] > new_points[-1][0] + offset or point[1] > new_points[-1][1] + offset or point[0] < new_points[-1][0] - offset or point[1] < new_points[-1][1] - offset]
 		
-	return new_points
+		return new_points
+	elif filter_type == "grid":
+		detected = {}
+		for row,col,del2,grid in points:
+			if grid not in detected:
+				detected[grid] = (row,col,del2)
+		return detected.values()
+	else:
+		print("Type not identified")
+		return []
 
 #Constructs a rectangle around the corner point. Size of the rectangle is the size of the neighbourhood
 #Takes in the image, corner point co-ordinates and the offset for neighbourhood
@@ -52,22 +66,25 @@ def draw_rectangle(img,x,y,offset):
 #Represents corners on the image and displays it on a plot
 #Takes in the image, corner points and offset of neighbourhood
 def display_corners(img,corners,offset):
-	for x,y,z in corners:
-		draw_rectangle(img,x,y,offset)
+	for row,col,delta,grid in corners:
+		draw_rectangle(img,row,col,offset)
 	
 	plt.gray()
+	plt.axis("off")
 	plt.imshow(img)
 	
 	plt.show()
 		
 #Performs corner detection
 #Takes in the input image, standard deviation value for gaussian smoothing, threshold value for corners and size of neighbourhood
-def detect_corners(img,sigma,threshold,n):
+def detect_corners(img,sigma=1,threshold=0.1,n=4,filter_type="linear"):
 	possible_corners = []
 	#Determine number of pixels and offset for neighbourhood
 	size = 2*n +1
 	offset=size//2
-	
+	row_grids = (len(img) - offset * 2) // size
+	column_grids = (len(img[0]) - offset * 2) // size
+
 	#Gaussian smoothing
 	smooth_img = ndimage.gaussian_filter(img,sigma)
 
@@ -84,14 +101,14 @@ def detect_corners(img,sigma,threshold,n):
 			eigenVals = np.linalg.eig(c_matrix)
 			#Smaller eigenvalue delta 2
 			del2 = min(eigenVals[0])
-			possible_corners.append([i,j,del2])
-	corners = filter_points(possible_corners,threshold,size)
+			possible_corners.append([i,j,del2,(i//size, j//size)]) #i//size,j//size gives grid co-ordinates
+	corners = filter_points(possible_corners,threshold,size,filter_type)
 	display_corners(smooth_img,corners,offset)
 	#print(corners)
 	
 #Test cases
 if __name__ == "__main__":
 	img = cv2.imread("CheckerBoard.jpg",0)
-	detect_corners(img,1,0.1,4)
+	detect_corners(img)
 	img1 = cv2.imread("Building1.jpg",0)
-	detect_corners(img1,1,0.1,4)
+	detect_corners(img1)
